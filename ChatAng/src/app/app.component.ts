fa94052628b3a6c5ee181e5d289ed0a0ce2745d8
Luke Liukonen <liukonen@gmail.com>
2020-07-09 15:47:19 -0500

    lets try to run this

--- ChatAng/src/app/app.component.ts
+++ ChatAng/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ChatAng';
+  title = 'River - RiveScript based chatbot in Angular';
 }
