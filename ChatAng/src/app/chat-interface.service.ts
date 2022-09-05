d52bc304da54e32895074df8116fca4be7b99c66
luke liukonen <liukonen@gmail.com>
2022-09-05 18:33:21 -0500

    remove references of heroku, since there is no more free tier

--- ChatAng/src/app/chat-interface.service.ts
+++ ChatAng/src/app/chat-interface.service.ts
@@ -11,16 +11,7 @@ export class ChatInterfaceService {
   constructor(private http: HttpClient) { }
 
   Talk(message){
-    try{
       return this.http.get<ChatResponse>(this.hostUrl, { params: new HttpParams().set('text', message), responseType: "json" });
-    }catch{
-      const headers= new HttpHeaders()
-      .set('Bypass-Tunnel-Reminder', '*')
-      .set('User-Agent', 'Chatbot');
-      return this.http.get<ChatResponse>(this.hostUrlBackup, { params: new HttpParams().set('text', message), responseType: "json" });
     }
-    }
-
 hostUrl = "https://bot.liukonen.dev";
-hostUrlBackup = "https://river-backend.herokuapp.com";
 }
